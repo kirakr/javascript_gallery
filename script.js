989cbb5dfd2de755802917efac700e426ed2f6fe
kirakr <82437235+kirakr@users.noreply.github.com>
2023-10-16 15:06:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+
+// 確認
+// console.log(main);
+// console.log(thumbs);
+
+const text = document.querySelector('#caption');
+
+// 繰り返しの処理
+thumbs.forEach(function(item, index) {
+    item.onmouseover= function() {
+    //   console.log(this.dataset.image);
+    main.src = this.dataset.image;
+    text.textContent = this.title;
+    }
+  });
